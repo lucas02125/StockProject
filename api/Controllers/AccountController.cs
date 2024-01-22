ff3f0560d367726f5997c98c092da5030204f263
Michael Lucas <lucas.michael02125@gmail.com>
2024-01-22 17:51:53 +0000

    Registering User via JWT

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using api.Dto;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -19,7 +20,44 @@ namespace api.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> ResisterUser([FromBody] UpdateAccountDto accountDto)
         {
+            try
+            {
 
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("This is a bad request");
+                }
+
+                var theUser = new AppUser()
+                {
+                    UserName = accountDto.UserName,
+                    Email = accountDto.EmailAddress
+                };
+
+                var addedUser = await _userManager.CreateAsync(theUser, accountDto.Password);
+
+                if (addedUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(theUser, "User");
+                    if (roleResult.Succeeded)
+                    {
+                        return Ok("User has been added");
+                    }
+                    else
+                    {
+                        return BadRequest(roleResult.Errors);
+                    }
+                }
+                else
+                {
+                    return BadRequest(addedUser.Errors);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.StackTrace);
+            }
         }
     }
 }
---
+++ api/Dto/Account/UpdateAccountDto.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Dto
+{
+
+    public class UpdateAccountDto
+    {
+        [Required]
+        public string? UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+    }
+
+}
