728358fb34b7e6c0745fcbe7bc534c1c8e96266a
Michael Lucas <lucas.michael02125@gmail.com>
2024-02-06 12:27:40 +0000

    Adding adttional tables and rows using Tailwind

---
+++ frontend/src/Components/IncomeStatement/IncomeStatement.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Interface } from "readline";
+
+interface Props {}
+
+const IncomeStatement = (props: Props) => {
+  return <div>IncomeStatement</div>;
+};
+
+export default IncomeStatement;
---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Table from "../../Components/Table/Table";
+
+type Props = {};
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+      <h1>DesignPage</h1>
+      <h2>This is a design page for you</h2>
+      <Table />
+    </>
+  );
+};
+
+export default DesignPage;
