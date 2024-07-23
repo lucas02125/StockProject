428a8481a9ac2b8546ff8991a93f8cdbea99c08f
Michael Lucas <lucas.michael02125@gmail.com>
2024-07-23 12:27:33 +0100

    - Adding in a comment box to put in for stocks

---
+++ frontend/src/Services/CommentService.tsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { CommentPost } from "../Models/Comment";
+import { handleError } from "../Helpers/ErrorHandlers";
+
+const api = "http://localhost:5167/api/comment/";
+
+export const commentPostAPI = async (
+  title: string,
+  content: string,
+  symbol: string
+) => {
+  try {
+    const data = await axios.post<CommentPost>(api + `${symbol}`, {
+      title: title,
+      content: content,
+    });
+
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export default commentPostAPI;
