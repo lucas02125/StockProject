e13501f6bc6a01a991232fb849ebab010573e258
Michael Lucas <lucas.michael02125@gmail.com>
2024-07-23 13:07:07 +0100

    - Adding in a list of Comments when posted onto a stock

--- frontend/src/Components/StockComment/StockComment.tsx
+++ frontend/src/Components/StockComment/StockComment.tsx
@@ -1,28 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockCommentForm from "./StockCommentForm/StockCommentForm";
-import commentPostAPI from "../../Services/CommentService";
+import commentPostAPI, { getCommentAPI } from "../../Services/CommentService";
 import { toast } from "react-toastify";
+import { CommentGet } from "../../Models/CommentGet";
+import StockCommentList from "../StockCommentList/StockCommentList";
+import Spinner from "../Spinner/Spinner";
 
 type Props = { stockSymbol: string };
+
 type CommentFormInputs = {
   title: string;
   content: string;
 };
 
 const StockComment = ({ stockSymbol }: Props) => {
+  const [comments, setComments] = useState<CommentGet[] | null>(null);
+  const [loading, setLoading] = useState<boolean>();
+  useEffect(() => {
+    getComments();
+  }, []);
   const handleComment = (e: CommentFormInputs) => {
     commentPostAPI(e.title, e.content, stockSymbol)
       .then((res) => {
         if (res) {
           toast.success("Comment created successfully!");
+          getComments();
         }
       })
       .catch((e) => {
         toast.warning(e);
       });
   };
+
+  const getComments = () => {
+    setLoading(true);
+    getCommentAPI(stockSymbol).then((res) => {
+      setLoading(false);
+      setComments(res?.data!);
+    });
+  };
+
   return (
-    <StockCommentForm symbol={stockSymbol} handleComment={handleComment} />
+    <div className="flex flex-col">
+      {loading ? <Spinner /> : <StockCommentList comments={comments!} />}
+      <StockCommentForm symbol={stockSymbol} handleComment={handleComment} />
+    </div>
   );
 };
 
--- frontend/src/Services/CommentService.tsx
+++ frontend/src/Services/CommentService.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { CommentPost } from "../Models/Comment";
 import { handleError } from "../Helpers/ErrorHandlers";
+import { CommentGet } from "../Models/CommentGet";
 
 const api = "http://localhost:5167/api/comment/";
 
@@ -14,6 +15,16 @@ export const commentPostAPI = async (
       title: title,
       content: content,
     });
+
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getCommentAPI = async (symbol: string) => {
+  try {
+    const data = await axios.get<CommentGet[]>(api + `?Symbol=${symbol}`, {});
 
     return data;
   } catch (error) {
